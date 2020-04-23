da6497b5dc1502a9b2490d050c5cfdafc08f574e
swpw <pawel2070202@wp.pl>
2020-04-23 18:46:37 +0200

    ADD: Basic three.js set up

--- src/js/THREE.js
+++ src/js/THREE.js
@@ -1,1 +1,88 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+const createCam = (fov = 75, near = 0.1, far = 1000) =>
+  new THREE.PerspectiveCamera(fov, window.innerWidth/window.innerHeight, near, far);
+
+
+let scene,
+  renderer,
+  camera,
+  controls,
+  loader
+
+
+// SCENE //
+scene = new THREE.Scene()
+
+
+// RENDERER //
+renderer = new THREE.WebGLRenderer({antialias: true});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setClearColor(0x7ec0ee, 1);
+
+renderer.gammaFactor = 1.5;
+renderer.gammaInput = true;
+renderer.gammaOutput = true;
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+document.body.appendChild( renderer.domElement );
+
+
+// CAMERA //
+camera = createCam(40)
+camera.position.set(5, 5, 5)
+scene.add(camera)
+
+// CONTROLS //
+controls = new OrbitControls( camera, renderer.domElement );
+
+
+// RESIZE
+window.addEventListener('resize', () => {
+  const { innerHeight, innerWidth } = window;
+
+  renderer.setSize(innerWidth, innerHeight);
+
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+
+// RENDER LOOP
+const onAnimationFrameHandler = time => {
+  renderer.render(scene, camera);
+
+  update(time)
+
+  window.requestAnimationFrame(onAnimationFrameHandler);
+}
+window.requestAnimationFrame(onAnimationFrameHandler);
+
+
+const update = (time) => {
+  controls.update();
+}
+
+
+/**/
+
+
+{
+  const light = new THREE.HemisphereLight('#fff', "#030522", 3)
+  scene.add(light)
+}
+
+
+{
+  const geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( {color: '#f00'} );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+}
